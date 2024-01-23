50d7147da98a7b09d7e7213745acbdb0cbad59e2
jinyounghwa <timotolkie@gmail.com>
2024-01-23 15:56:24 +0900

    styleXjs_navigationBar

--- stylex/app/dashboard/layout.tsx
+++ stylex/app/dashboard/layout.tsx
@@ -1,17 +1,26 @@
 import stylex from "@stylexjs/stylex";
 
-const s = stylex.create({
-  main: {
+const header = stylex.create({
+   navigationItem: {
     display: "flex",
-    flexDirection: "column",
     alignItems: "center",
-    gap: 10,
-    paddingTop: 30,
-    fontSize: 30,
-    fontStyle: "bold",
+    padding: 10 | 10 | 10 | 10,
+    justifyContent: "space-between",
+    backgroundColor: "white",
+    position: "fixed",
+    width: "100%",
+    height: 50,
+    boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
   },
-});
-
+  textstyle: {
+    display: "inline-flex",
+    listStyleType: "none",
+    margin: "auto",
+    backgroundPosition: "center",
+    gap: 100,
+  },
+  }
+);
 
 export default function DashboardLayout({
   children, // will be a page or nested layout
@@ -20,8 +29,21 @@ export default function DashboardLayout({
 }) {
   return (
     <section>
-    <nav>
-        <div className={stylex(s.main)}>hi</div>
+      <nav>
+        <div className={ stylex(header.navigationItem)}>
+          <ul className={ stylex(header.textstyle)}>
+          <li>새로운 이야기</li>
+          <li>지나간 이야기</li>
+          <li>SoundCloud</li>
+          <li>출석부</li>
+          <li>커뮤니티</li>
+          <li>컨텐츠들</li>
+          </ul>
+          <div >
+            <a href="#">Login</a>
+            <a href="#">Sign Up</a>
+          </div>
+          </div>
       </nav>
       {children}
     </section>
